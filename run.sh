b868b020d25802c8cee2df170e3894e88f399dc6
Ubuntu <ubuntu@ip-10-215-137-100.us-west-2.compute.internal>
2014-09-24 13:06:59 +0000

    Install java sdk.

--- run.sh
+++ run.sh
@@ -2,11 +2,15 @@
 
 cd $(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
+echo "Install dependencies"
+sudo apt-get -qq update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -y install openjdk-7-jdk 
+
 echo "Setting up leiningen"
-if [ ! -f lein ];
+if [ ! -f /usr/local/bin/lein ];
 then
-    wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-    chmod +x lein
+	sudo wget -P /usr/local/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+	sudo chmod +x /usr/local/lein
 fi
 export PATH=$PWD:$PATH
 
