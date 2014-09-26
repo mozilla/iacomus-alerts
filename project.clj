68b91d4f91f92035e171a6756fa8bfddd7fe63d6
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-09-26 18:20:20 +0000

    Fix typo.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main "iacomus-alerts.core"
+  :main iacomus-alerts.core
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [clj-time "0.8.0"]
